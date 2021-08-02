0e275e3c75d6aa3efd5acaaaa0de3d6bfa76beda
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-02 19:03:48 +0500

    Add function to open menu

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ const options = document.querySelectorAll('.option');
 const btnCloseMenu = document.querySelector('.close-menu');
 const btnOpenMenu = document.querySelector('.open-menu');
 
-
+function openMenu() {
+  overlay.classList.remove('hidden');
+}
