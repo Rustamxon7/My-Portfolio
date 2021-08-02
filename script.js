8b84015fb85fd22d17553e6b452e445bef661f3c
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2021-08-02 18:12:14 +0500

    Prepare for pair programming

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-'use strict';
+const overlay = document.querySelector('.overlay');
+const options = document.querySelectorAll('.option');
+const btnCloseMenu = document.querySelector('.close-menu');
+const btnOpenMenu = document.querySelector('.open-menu');
 
+function openMenu() {
+  overlay.classList.remove('hidden');
+}
 
-console.log('Hello');
+function closeMenu() {
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < options.length; i += 1) {
+  options[i].addEventListener('click', closeMenu);
+}
+
+btnOpenMenu.addEventListener('click', openMenu);
+btnCloseMenu.addEventListener('click', closeMenu);
